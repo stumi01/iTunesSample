44c6f859a4ae8821da22ef09a3ad7b8854c23c8a
Bence Stumpf <stumiw@gmail.com>
2018-11-25 13:01:01 +0100

    Song row design

--- app/src/main/java/com/bencestumpf/itunessample/presentation/search/SongsAdapter.kt
+++ app/src/main/java/com/bencestumpf/itunessample/presentation/search/SongsAdapter.kt
@@ -9,6 +9,7 @@ import com.bumptech.glide.Glide
 import com.hannesdorfmann.annotatedadapter.annotation.ViewField
 import com.hannesdorfmann.annotatedadapter.annotation.ViewType
 import com.hannesdorfmann.annotatedadapter.support.recyclerview.SupportAnnotatedAdapter
+import java.text.SimpleDateFormat
 
 class SongsAdapter(context: Context, private val onSongClick: (Long) -> Unit) : SupportAnnotatedAdapter(context),
     SongsAdapterBinder {
@@ -19,12 +20,17 @@ class SongsAdapter(context: Context, private val onSongClick: (Long) -> Unit) : 
         layout = R.layout.view_song_row,
         views = [ViewField(id = R.id.song_title, name = "title", type = TextView::class),
             ViewField(id = R.id.song_artist, name = "artist", type = TextView::class),
-            ViewField(id = R.id.song_details, name = "details", type = TextView::class),
+            ViewField(id = R.id.song_genre, name = "genre", type = TextView::class),
             ViewField(id = R.id.song_album, name = "album", type = TextView::class),
+            ViewField(id = R.id.song_price, name = "price", type = TextView::class),
+            ViewField(id = R.id.song_length, name = "length", type = TextView::class),
+            ViewField(id = R.id.song_release_date, name = "releaseDate", type = TextView::class),
             ViewField(id = R.id.song_image, name = "image", type = ImageView::class)
         ]
     )
     val songRow: Int = 0
+
+    private val dateFormat = SimpleDateFormat("yyyy-MM-dd")
 
     override fun getItemCount(): Int {
         return songs.size
@@ -34,9 +40,13 @@ class SongsAdapter(context: Context, private val onSongClick: (Long) -> Unit) : 
         vh?.let {
             val song = songs[position]
             it?.title.text = song.title
+            it?.length.text = song.trackTimeMillis?.toReadable()
             it?.artist.text = song.artistName
-            it?.details.text = song.genre
             it?.album.text = song.album
+            it?.genre.text = song.genre
+            it?.releaseDate.text = dateFormat.format(song.releaseDate)
+            it?.price.text = "${song.trackPrice} ${song.currency}"
+
 
             Glide.with(it.image)
                 .load(song.artworkThumbnailUrl)
@@ -52,4 +62,10 @@ class SongsAdapter(context: Context, private val onSongClick: (Long) -> Unit) : 
         notifyDataSetChanged()
     }
 
+}
+
+private fun Long.toReadable(): String {
+    val minutes = this / 1000 / 60
+    val seconds = this / 1000 % 60
+    return "$minutes:$seconds"
 }
